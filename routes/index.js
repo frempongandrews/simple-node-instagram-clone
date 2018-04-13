8c12ba829d32b29a8474445e148f99d74094fdf4
Andrews Frempong <frempongandrews@yahoo.com>
2018-04-14 00:40:40 +0100

    error page completed

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 // Full Documentation - https://www.turbo360.co/docs
 const turbo = require('turbo360')({site_id: process.env.TURBO_APP_ID})
 const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
-const router = vertex.router()
-const axios = require("axios")
+const router = vertex.router();
+const axios = require("axios");
 
 /*  This is the home route. It renders the index.mustache page from the views directory.
 	Data is rendered using the Mustache templating engine. For more
@@ -11,33 +11,49 @@ router.get('/', function(req, res) {
 	const data = {
 		text: "Instagram Clone",
 		greeting: "Welcome!"
-	}
+	};
     res.render('index', data)
 
-})
+});
 
 router.get("/:username", function (req, res) {
 
-	const username = req.params.username
-	const instagramAPI = "https://www.instagram.com/" + username + "/?__a=1"
-    var instagramData;
-    axios.get(instagramAPI)
-        .then( function (response) {
+	const username = req.params.username;
+	if (username === "lusso_africano") {
 
-            instagramData = response.data.graphql
-            res.render("index", instagramData)
-            console.log(instagramData)
+        const instagramAPI = "https://firebasestorage.googleapis.com/v0/b/instagram-clone-nodejs.appspot.com/o/inst.json?alt=media&token=b0de75f1-01d9-47b3-b066-1df716d1f318";
+        var instagramData;
+        axios.get(instagramAPI)
+            .then( function (response) {
 
-        })
-        .catch (function (error) {
-            res.json({
-                confirmation: "fail",
-                message: error.message
+                instagramData = response.data.graphql;
+                res.render("index", instagramData);
+
+
+                console.log(instagramData)
+
             })
-            //console.log(error.message)
-        })
+            .catch (function (error) {
+                res.json({
+                    confirmation: "fail",
+                    message: error.message
+                })
 
-})
+                //console.log(error.message);
+            });
+
+    } else {
+
+	    res.render("error", {
+	        confirmation: "Fail",
+            errorMessage: "Sorry, this page isn't available.",
+            errorSubMessage: "The link you followed may be broken, or the page may have been removed.",
+            homeLinkText: "Go back to Instagram."
+        });
+    }
 
 
-module.exports = router
+});
+
+
+module.exports = router;
